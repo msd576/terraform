5229ddb05cfaa37ed12b024955555e7b40b42e04
Mitra <155680989+msd576@users.noreply.github.com>
2024-02-03 19:06:42 -0600

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "vm" {
+  ami           = "<ami>"
+  subnet_id     = "<subnet>"
+  instance_type = "t3.micro"
+  tags = {
+    Env = "dev"
+  }
+}
