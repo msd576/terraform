59734a3c6352a4ed619dd5cee35705a5bc8ed393
Mitra <155680989+msd576@users.noreply.github.com>
2024-02-08 18:54:04 -0600

    Create backend.tf

---
+++ docker-nginx-s3/backend.tf
@@ -0,0 +1,15 @@
+#s3 backend for terraform state file
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+  required_version = ">= 0.13"
+  backend "s3" {
+    profile = "demo"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    bucket  = "<AWS-S3-BUCKET>"
+  }
+}
